a1a1418b6a7a3bc515ee61e90a818b8b5ca124e9
Phong Do <phongdo@innoria.com>
2018-12-19 23:44:48 +0700

    add question

--- src/components/create_form/form_data.ts
+++ src/components/create_form/form_data.ts
@@ -97,4 +97,240 @@ export const formData: QAInterface[] = [
             }
         ]
     },
+    {
+        question: {
+            width: 343,
+            height: 172,
+            x: 0,
+            y: 446
+        },
+        answers: [
+            {
+                width: 337,
+                height: 18,
+                x: 2,
+                y: 25
+            },
+            {
+                width: 337,
+                height: 18,
+                x: 2,
+                y: 44
+            },
+            {
+                width: 337,
+                height: 18,
+                x: 2,
+                y: 63
+            },
+            {
+                width: 337,
+                height: 16,
+                x: 2,
+                y: 82
+            },
+            {
+                width: 337,
+                height: 16,
+                x: 2,
+                y: 99
+            },
+            {
+                width: 337,
+                height: 16,
+                x: 2,
+                y: 116
+            },
+            {
+                width: 337,
+                height: 16,
+                x: 2,
+                y: 133
+            },
+            {
+                width: 337,
+                height: 16,
+                x: 2,
+                y: 150
+            }
+        ]
+    },
+    {
+        question: {
+            width: 445,
+            height: 172,
+            x: 0,
+            y: 621
+        },
+        answers: [
+            {
+                width: 439,
+                height: 18,
+                x: 2,
+                y: 25
+            },
+            {
+                width: 439,
+                height: 18,
+                x: 2,
+                y: 44
+            },
+            {
+                width: 439,
+                height: 18,
+                x: 2,
+                y: 63
+            },
+            {
+                width: 439,
+                height: 16,
+                x: 2,
+                y: 82
+            },
+            {
+                width: 439,
+                height: 16,
+                x: 2,
+                y: 99
+            },
+            {
+                width: 439,
+                height: 16,
+                x: 2,
+                y: 116
+            },
+            {
+                width: 439,
+                height: 16,
+                x: 2,
+                y: 133
+            },
+            {
+                width: 439,
+                height: 16,
+                x: 2,
+                y: 150
+            }
+        ]
+    },
+    {
+        question: {
+            width: 445,
+            height: 172,
+            x: 0,
+            y: 796
+        },
+        answers: [
+            {
+                width: 439,
+                height: 18,
+                x: 2,
+                y: 25
+            },
+            {
+                width: 439,
+                height: 18,
+                x: 2,
+                y: 44
+            },
+            {
+                width: 439,
+                height: 18,
+                x: 2,
+                y: 63
+            },
+            {
+                width: 439,
+                height: 34,
+                x: 2,
+                y: 82
+            },
+            {
+                width: 439,
+                height: 16,
+                x: 2,
+                y: 117
+            },
+            {
+                width: 439,
+                height: 34,
+                x: 2,
+                y: 134
+            }
+        ]
+    },
+    {
+        question: {
+            width: 424,
+            height: 147,
+            x: 0,
+            y: 975
+        },
+        answers: [
+            {
+                width: 419,
+                height: 18,
+                x: 2,
+                y: 36
+            },
+            {
+                width: 419,
+                height: 18,
+                x: 2,
+                y: 55
+            },
+            {
+                width: 419,
+                height: 34,
+                x: 2,
+                y: 74
+            },
+            {
+                width: 419,
+                height: 16,
+                x: 2,
+                y: 109
+            },
+            {
+                width: 419,
+                height: 16,
+                x: 2,
+                y: 126
+            }
+        ]
+    },
+    {
+        question: {
+            width: 804,
+            height: 78,
+            x: 0,
+            y: 1129
+        },
+        answers: [
+            {
+                width: 474,
+                height: 25,
+                x: 2,
+                y: 21
+            },
+            {
+                width: 474,
+                height: 25,
+                x: 2,
+                y: 48
+            },
+            {
+                width: 323,
+                height: 25,
+                x: 478,
+                y: 21
+            },
+            {
+                width: 323,
+                height: 25,
+                x: 478,
+                y: 48
+            }
+        ]
+    },
 ];
